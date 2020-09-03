62a8e29a4ab5fead9e124a7e91d10a9b9d33bffd
FarDean <sfh1212@gmail.com>
2020-09-03 19:12:59 +0430

    pfff

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React,{useState} from 'react';
 import './App.css';
 import useFetchJobs from './useFetchJobs';
 import {Container} from 'react-bootstrap'
-
+import SearchForm from "./SearchForm";
 import Job from "./Job";
 import JobsPagination from './JobsPagination';
+
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const {jobs,loading,error,hasNextPage} = useFetchJobs(params,page)
+
+  function handleParamChange(e){
+    const param = e.target.className
+    const value = e.target.value
+    setPage(1)
+    setParams(prev=>{
+      return {...prev,[param]:value}
+    })
+  }
+
   return (
     <Container className="my-4">
         <h1 className="mb-4">Github Jobs</h1>
+        <SearchForm params={params} onParamChange={handleParamChange} />
         <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
         {loading && <h1>loading...</h1>}
         {error && <h1>error.Refresh.</h1>}
